eca9ba0c6e08aeace050ee27992e6653a083ea4e
zuvizudar <zuvizudar@yahoo.co.jp>
2020-05-13 15:22:22 +0900

    heroku修正

--- models/loader.js
+++ models/loader.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 //const sequelize = new Sequelize('postgres://postgres:postgres@localhost/physics_simulator');
 const sequelize = new Sequelize(
-    process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/2d-simulator'
+    process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/physics-simulator'
 );
 
 module.exports ={
